bf4c993a23b711ce62f118d97ded687a006ca097
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-04-29 17:53:28 -0300

    Alterada logica no index para começar o jogo com a função startGame

--- snake-game/index.js
+++ snake-game/index.js
@@ -1,16 +1,20 @@
 const SCALE = 15;
+const tabuleiroCanvas = document.getElementById('game-board');
+let tabuleiro;
+let renderer;
+let gameLoop;
 
 function init() {
-  const tabuleiroCanvas = document.getElementById('game-board');
-  
-  const tabuleiro = new Tabuleiro(tabuleiroCanvas, SCALE);
+  tabuleiro = new Tabuleiro(tabuleiroCanvas, SCALE);
   tabuleiro.jogador = new Jogador(5, 5);
   
-  const renderer = new Renderer(tabuleiro, tabuleiroCanvas);
+  renderer = new Renderer(tabuleiro, tabuleiroCanvas);
   renderer.setScale(SCALE);
 
-  const gameLoop = new GameLoop(new Game(tabuleiro), renderer);
+  gameLoop = new GameLoop(new Game(tabuleiro), renderer);
+}
 
+function startGame() {
   gameLoop.bootstrap();
   gameLoop.start();
 }
