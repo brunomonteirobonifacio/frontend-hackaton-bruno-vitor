57817173c899b7c937b23c9b9dab2bef5c60dd10
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-04-29 17:27:44 -0300

    Simplificada lógica em index.js

--- snake-game/index.js
+++ snake-game/index.js
@@ -4,46 +4,15 @@ function init() {
   const tabuleiroCanvas = document.getElementById('game-board');
   
   const tabuleiro = new Tabuleiro(tabuleiroCanvas, SCALE);
-  const jogador = new Jogador(5, 5);
-  tabuleiro.jogador = jogador;
+  tabuleiro.jogador = new Jogador(5, 5);
   
   const renderer = new Renderer(tabuleiro, tabuleiroCanvas);
   renderer.setScale(SCALE);
 
-  const game = new Game(tabuleiro);
-  game.addStepObserver(renderer);
-  
-  observeTeclado(game);
-  console.log(Direcoes.ESQUERDA.isOposta(Direcoes.DIREITA))
-}
+  const gameLoop = new GameLoop(new Game(tabuleiro), renderer);
 
-function observeTeclado(game) {
-  document.addEventListener('keydown', (event) => {
-    const tecladoAction = TECLADO_ACTIONS[event.key];
-
-    if (tecladoAction) {
-      tecladoAction(game);
-
-      if (!game.isRunning()) {
-        game.start();
-      }
-    }
-  })
-}
-
-const TECLADO_ACTIONS = {
-  ArrowRight: (game) => {
-    game.tabuleiro.jogador.mudarDirecao(Direcoes.DIREITA);
-  },
-  ArrowLeft: (game) => {
-    game.tabuleiro.jogador.mudarDirecao(Direcoes.ESQUERDA);
-  },
-  ArrowUp: (game) => {
-    game.tabuleiro.jogador.mudarDirecao(Direcoes.CIMA);
-  },
-  ArrowDown: (game) => {
-    game.tabuleiro.jogador.mudarDirecao(Direcoes.BAIXO);
-  }
+  gameLoop.bootstrap();
+  gameLoop.start();
 }
 
 init();
